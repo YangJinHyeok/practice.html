81b3540eeb04e3b3fed7cfb39e2137efe6dd4c33
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2022-08-18 19:53:18 +0900

    Add files via upload

---
+++ 8주차/스톱워치/script1.js
@@ -0,0 +1,56 @@
+let minutes = 0;
+let seconds = 0;
+let tenMillis = 0;
+const appendTens = document.getElementById("tenMillis");
+const appendSeconds = document.getElementById("seconds");
+const appendMinutes = document.getElementById("minutes");
+const buttonStart = document.getElementById("bt__start");
+const buttonStop = document.getElementById("bt__stop");
+const buttonReset = document.getElementById("bt__reset");
+const buttonRecord = document.getElementById("bt__record");
+const recordList =  document.getElementById("recordList");
+let intervalId;
+let records = "";
+
+buttonStart.onclick = function(){
+  clearInterval(intervalId)
+  intervalId = setInterval(operateTimer, 10)
+}
+
+buttonStop.onclick = function(){
+  clearInterval(intervalId)
+}
+
+buttonReset.onclick = function(){
+  clearInterval(intervalId)
+  tenMillis = 0; seconds = 0; minutes = 0;
+  appendTens.textContent = "00"
+  appendSeconds.textContent = "00"
+  appendMinutes.textContent = "00"
+  records = ""
+  recordList.innerText = ""
+}
+
+buttonRecord.onclick = function(){
+  records += appendMinutes.innerText + ":"
+  records += appendSeconds.innerText + ":"
+  records += appendTens.innerText + "\n"
+  document.getElementById("recordList").innerText = records
+}
+
+function operateTimer(){
+  tenMillis++;
+  appendTens.textContent = tenMillis > 9 ? tenMillis : '0' + tenMillis
+  if(tenMillis>99){
+    seconds++;
+    appendSeconds.textContent = seconds > 9 ? seconds : '0' + seconds
+    tenMillis = 0
+    appendTens.textContent = "00"
+  }
+  if(seconds > 59){
+    minutes++;
+    appendMinutes.textContent = minutes > 9 ? minutes : '0' + minutes
+    seconds = 0
+    appendSeconds.textContent = "00"
+  }
+}
